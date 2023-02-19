b0c40fa24cdfcde602f7899c6a618be3a477f364
Stefan Maaßen <stefan.maassen@posteo.de>
2023-02-20 00:15:43 +0100

    added kitty and rust

--- config.lua
+++ config.lua
@@ -181,6 +181,10 @@ formatters.setup {
     name = "taplo",
     filetypes = { "toml" },
   },
+  {
+    name = "rustfmt",
+    filetypes = { "rust" },
+  },
 }
 
 local linters = require "lvim.lsp.null-ls.linters"
@@ -762,6 +766,26 @@ lvim.plugins = {
       }
       -- vim.keymap.set({ "n", "x", "o" }, "<leader>s",
       -- end, {})
+    end,
+  },
+  {
+    "fladson/vim-kitty",
+  },
+  {
+    "simrat39/rust-tools.nvim",
+    requires = { "nvim-lua/plenary.nvim", "mfussenegger/nvim-dap" },
+    config = function()
+      local rt = require "rust-tools"
+      rt.setup {
+        server = {
+          on_attach = function(_, bufnr)
+            -- Hover actions
+            vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
+            -- Code action groups
+            vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+          end,
+        },
+      }
     end,
   },
 }
