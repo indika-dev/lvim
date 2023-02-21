3e022e8f8f1f05b37789c5185a023d100067ffed
Stefan Maaßen <s.maassen@verband.creditreform.de>
2023-02-21 10:19:13 +0100

    added glow plugin

--- config.lua
+++ config.lua
@@ -764,6 +764,23 @@ lvim.plugins = {
       -- end, {})
     end,
   },
+  {
+    "ellisonleao/glow.nvim",
+    config = function()
+      local status_ok, glow = pcall(require, "glow")
+      if status_ok then
+        glow.setup {
+          border = "shadow", -- floating window border config
+          style = "dark|light", -- filled automatically with your current editor background, you can override using glow json style
+          pager = false,
+          width = 80,
+          height = 100,
+          width_ratio = 0.7, -- maximum width of the Glow window compared to the nvim window size (overrides `width`)
+          height_ratio = 0.7,
+        }
+      end
+    end,
+  },
 }
 
 if vim.g.neovide then
