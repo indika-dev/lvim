22726b1b26937926c7d1fe1df36948bdfc24c8ee
Stefan Maaßen <stefan.maassen@posteo.de>
2023-03-11 20:46:31 +0100

    added noice to config

--- config.lua
+++ config.lua
@@ -224,6 +224,10 @@ linters.setup {
     filetypes = { "yaml", "yml" },
   },
   {
+    command = "jsonlint",
+    filetypes = { "json" },
+  },
+  {
     command = "eslint_d",
     filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
   },
@@ -602,9 +606,11 @@ lvim.plugins = {
   --   after = { "mason.nvim", "null-ls.nvim" },
   --   config = function()
   --     require("mason-null-ls").setup {
-  --       automatic_installation = false,
+  --       automatic_installation = true,
   --       automatic_setup = true,
-  --       ensure_installed = {},
+  --       ensure_installed = {
+  --         "jsonlint",
+  --       },
   --     }
   --     require("mason-null-ls").setup_handlers()
   --   end,
@@ -842,38 +848,7 @@ lvim.plugins = {
   {
     "folke/noice.nvim",
     config = function()
-      require("noice").setup {
-        lsp = {
-          -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
-          override = {
-            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-            ["vim.lsp.util.stylize_markdown"] = true,
-            ["cmp.entry.get_documentation"] = true,
-          },
-          -- signature = {
-          --   enabled = false,
-          -- },
-          progress = {
-            enabled = false,
-            -- Lsp Progress is formatted using the builtins for lsp_progress. See config.format.builtin
-            -- See the section on formatting for more details on how to customize.
-            --- @type NoiceFormat|string
-            format = "lsp_progress",
-            --- @type NoiceFormat|string
-            format_done = "lsp_progress_done",
-            throttle = 1000 / 30, -- frequency to update lsp progress message
-            view = "mini",
-          },
-        },
-        -- you can enable a preset for easier configuration
-        presets = {
-          bottom_search = true, -- use a classic bottom cmdline for search
-          command_palette = true, -- position the cmdline and popupmenu together
-          long_message_to_split = true, -- long messages will be sent to a split
-          inc_rename = false, -- enables an input dialog for inc-rename.nvim
-          lsp_doc_border = false, -- add a border to hover docs and signature help
-        },
-      }
+      require("user.noice").config()
     end,
     requires = {
       -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
@@ -883,6 +858,18 @@ lvim.plugins = {
       --   If not available, we use `mini` as the fallback
       "rcarriga/nvim-notify",
     },
+  },
+  {
+    "RishabhRD/nvim-cheat.sh",
+    dependencies = "RishabhRD/popfix",
+    config = function()
+      vim.g.cheat_default_window_layout = "vertical_split"
+    end,
+    lazy = true,
+    cmd = { "Cheat", "CheatWithoutComments", "CheatList", "CheatListWithoutComments" },
+    keys = "<leader>?",
+    -- enabled = lvim.builtin.cheat.active,
+    disable = true,
   },
 }
 
