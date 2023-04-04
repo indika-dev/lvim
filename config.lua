7d97d59215269c0b2f0c1b7e2192a5c55a727749
Stefan Maaßen <s.maassen@verband.creditreform.de>
2023-04-04 11:49:32 +0200

    updated config

--- config.lua
+++ config.lua
@@ -948,49 +948,52 @@ lvim.plugins = {
       -- "preservim/vim-markdown",
       -- "godlygeek/tabular",
     },
-    opts = {
-      completion = {
-        nvim_cmp = true, -- if using nvim-cmp, otherwise set to false
-      },
-      note_id_func = function(title)
-        -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
-        local suffix = ""
-        if title ~= nil then
-          -- If title is given, transform it into valid file name.
-          suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
-        else
-          -- If title is nil, just add 4 random uppercase letters to the suffix.
-          for _ = 1, 4 do
-            suffix = suffix .. string.char(math.random(65, 90))
+    opts = function()
+      local options = {
+        completion = {
+          nvim_cmp = true, -- if using nvim-cmp, otherwise set to false
+        },
+        note_id_func = function(title)
+          -- Create note IDs in a Zettelkasten format with a timestamp and a suffix.
+          local suffix = ""
+          if title ~= nil then
+            -- If title is given, transform it into valid file name.
+            suffix = title:gsub(" ", "-"):gsub("[^A-Za-z0-9-]", ""):lower()
+          else
+            -- If title is nil, just add 4 random uppercase letters to the suffix.
+            for _ = 1, 4 do
+              suffix = suffix .. string.char(math.random(65, 90))
+            end
           end
-        end
-        return tostring(os.time()) .. "-" .. suffix
-      end,
-      templates = {
-        date_format = "%Y-%m-%d-%a",
-        time_format = "%H:%M",
-      },
-      note_frontmatter_func = function(note)
-        local out = { id = note.id, aliases = note.aliases, tags = note.tags }
-        -- `note.metadata` contains any manually added fields in the frontmatter.
-        -- So here we just make sure those fields are kept in the frontmatter.
-        if note.metadata ~= nil and require("obsidian").util.table_length(note.metadata) > 0 then
-          for k, v in pairs(note.metadata) do
-            out[k] = v
+          return tostring(os.time()) .. "-" .. suffix
+        end,
+        templates = {
+          date_format = "%Y-%m-%d-%a",
+          time_format = "%H:%M",
+        },
+        note_frontmatter_func = function(note)
+          local out = { id = note.id, aliases = note.aliases, tags = note.tags }
+          -- `note.metadata` contains any manually added fields in the frontmatter.
+          -- So here we just make sure those fields are kept in the frontmatter.
+          if note.metadata ~= nil and require("obsidian").util.table_length(note.metadata) > 0 then
+            for k, v in pairs(note.metadata) do
+              out[k] = v
+            end
           end
-        end
-        return out
-      end,
-      use_advanced_uri = false,
-    },
-    config = function(_, opts)
+          return out
+        end,
+        use_advanced_uri = false,
+      }
       if user == "stefan" then
-        opts.dir = "~/Dokumente/obsidian/my-vault"
-        opts.templates.subdir = "my-templates-folder"
+        options.dir = "~/Dokumente/obsidian/my-vault"
+        options.templates.subdir = "my-templates-folder"
       else
-        opts.dir = ""
-        opts.templates.subdir = "my-templates-folder"
+        options.dir = "~/Dokumente/obsidian-vault/Makroarchitektur"
+        options.templates.subdir = "Templates"
       end
+      return options
+    end,
+    config = function(_, opts)
       require("obsidian").setup(opts)
       require("nvim-treesitter.configs").setup {
         ensure_installed = { "markdown", "markdown_inline" },
